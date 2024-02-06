aaf85f1635c4c90b0a92b52cf854924139ad7633
Pierre Clabot <pierre.clabot@gmail.com>
2024-02-06 11:45:57 +0100

    update/ project complete

--- src/components/carousel/carousel.jsx
+++ src/components/carousel/carousel.jsx
@@ -30,17 +30,26 @@ function Carousel(props){
             <div className="carousel">
                 <img className="background" src={pictures[index]} alt="" />
                 <div className="arrow left" onClick={handleClick}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="80" viewBox="0 0 48 80" fill="black">
-                        <path d="M47.04 7.78312L39.92 0.703125L0.359985 40.3031L39.96 79.9031L47.04 72.8231L14.52 40.3031L47.04 7.78312Z" fill="white"/>
-                    </svg>
+                    {
+                        pictures.length > 1 ? (<svg xmlns="http://www.w3.org/2000/svg" width="48" height="80" viewBox="0 0 48 80" fill="black">
+                                                <path d="M47.04 7.78312L39.92 0.703125L0.359985 40.3031L39.96 79.9031L47.04 72.8231L14.52 40.3031L47.04 7.78312Z" fill="white"/>
+                                            </svg>):""
+                    }
+
                 </div>
                 <div className="arrow right" onClick={handleClick}>
-                    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="80" viewBox="0 0 48 80" fill="none">
+
+                    {
+                        pictures.length > 1 ? (                    <svg xmlns="http://www.w3.org/2000/svg" width="48" height="80" viewBox="0 0 48 80" fill="none">
                         <path d="M0.960022 72.3458L8.04002 79.4258L47.64 39.8258L8.04002 0.22583L0.960022 7.30583L33.48 39.8258L0.960022 72.3458Z" fill="white"/>
-                    </svg>
+                    </svg>):""
+                    }
                 </div>
                 <div className="index">
-                    {(index+1)+"/"+pictures.length}
+                    {
+                        pictures.length > 1 ? ((index+1)+"/"+pictures.length):""
+                    }
+                    
                 </div>
             </div>
         )
